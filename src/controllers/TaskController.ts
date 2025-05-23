8f8e827df233f614dbd7165b17d88f73cb999890
Willian Hercilio Pezzini <williantijucas@gmail.com>
2025-05-23 06:47:06 -0300

    conectando front e back

--- src/controllers/TaskController.ts
+++ src/controllers/TaskController.ts
@@ -39,22 +39,24 @@ class TaskController{
     };
 
     add(req: Request, res: Response) {
-        const {id, descricao, data, status} = req.body;
+        const {descricao, data, status} = req.body;
 
         //Validação dos dados, verifica se todos os dados a baixo estão corretamente preenchidos.
-        if(id && descricao && data && status) {
+        if(descricao && data && status) {
             if(status === 'fazer' || status ==='fazendo' || status ==='finalizado') {
-                 const result = taskService.add(req.body);
-                res.json(result);
+                const result = taskService.add(req.body);
                 res.status(201);
+                res.json(result);
+                
             }else {
                 res.json({error: "Status Invalido: fazer, fazendo ou finalizado"})
             }
            
         }else {
             //Se algum dados estiver errado, cau aqui neste else
-            res.json({error: "Parâmetro Inválido"});
             res.status(401);
+            res.json({error: "Parâmetro Inválido"});
+           
         }
     }
 
